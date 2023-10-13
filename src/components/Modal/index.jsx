73ce50afdfb83d37e71de12026c20d7cb2cedffe
deyvis <deyvisnvg@gmail.com>
2023-10-13 10:47:38 -0500

    feat(deyvisnvg): Modal transition

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,21 +1,34 @@
 import { Dialog, Transition } from "@headlessui/react"
 import PropTypes from "prop-types"
+import { Fragment } from "react"
 
 export default function Modal({ children, open, onClose }) {
     return (
-        <Dialog open={open} onClose={onClose} className="relative z-50">
-            <div className="fixed inset-0 bg-black/30"/>
-            <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
-                <Dialog.Panel className="w-full max-w-sm rounded bg-white p-4">
-                    {children}
-                </Dialog.Panel>
-            </div>
-        </Dialog>
+        <Transition show={open} as={Fragment}>
+            <Dialog open={open} onClose={onClose} className="relative z-50">
+                <Transition.Child
+                    as={Fragment}
+                    enter="ease-out duration-500"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="ease-in duration-400"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                >
+                    <div className="fixed inset-0 bg-black/30" />
+                </Transition.Child>
+                <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
+                    <Dialog.Panel className="w-full max-w-sm rounded bg-white p-4">
+                        {children}
+                    </Dialog.Panel>
+                </div>
+            </Dialog>
+        </Transition>
     )
 }
 
 Modal.prototype = {
     children: PropTypes.element,
     open: PropTypes.bool,
-    onClase: PropTypes.func
+    onClose: PropTypes.func
 }
